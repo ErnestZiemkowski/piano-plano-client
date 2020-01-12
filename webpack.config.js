213a83e05c7d06f85a08f20362bdf4783add045d
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2020-01-12 20:32:00 +0100

    Config fix

--- webpack.config.js
+++ webpack.config.js
@@ -51,5 +51,10 @@ module.exports = {
     contentBase: './dist',
     hot: true,
     historyApiFallback: true,
-  }
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+}
 };
